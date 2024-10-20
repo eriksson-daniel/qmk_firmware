a196189dca828a1b59c5c88adac1cee16ac1e5c2
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-10-20 18:55:11 +0200

    Avalanche

--- keyboards/avalanche/avalanche.c
+++ keyboards/avalanche/avalanche.c
@@ -7,19 +7,81 @@
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) {
-        if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
-        } else {
-            tap_code_delay(KC_VOLD, 10);
+        switch(biton32(layer_state)) {
+            case 1:
+                if (clockwise) {
+                    tap_code(KC_RGHT);
+                } else {
+                    tap_code(KC_LEFT);
+                }
+
+                return true;
+            case 4:
+                if (clockwise) {
+                    tap_code(KC_VOLD);
+                } else {
+                    tap_code(KC_VOLU);
+                }
+
+                return true;
+            default:
+                if (clockwise) {
+                    tap_code(KC_UP);
+                } else {
+                    tap_code(KC_DOWN);
+                }
+
+                return true;
+
         }
     } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_PGUP);
-        } else {
-            tap_code(KC_PGDN);
+        switch(biton32(layer_state)){
+            case 2:
+                if (clockwise) {
+                    register_code(KC_LEFT_SHIFT);
+                    tap_code(KC_TAB);
+                    unregister_code(KC_LEFT_SHIFT);
+                } else {
+                    tap_code(KC_TAB);
+                }
+            
+                return true;
+            case 4:
+                if (clockwise) {
+                    tap_code(KC_VOLD);
+                } else {
+                    tap_code(KC_VOLU);
+                }
+
+                return true;    
+            default:
+                if (clockwise) {
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                    tap_code(KC_UP);
+                } else {
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                    tap_code(KC_DOWN);
+                }
+
+                return true;
         }
     }
     return true;
 }
 #endif
-
--- keyboards/avalanche/v4/v4.c
+++ keyboards/avalanche/v4/v4.c
@@ -8,11 +8,43 @@ oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     return OLED_ROTATION_180;
 }
 
+enum Layer {
+    _BASE,
+    _FUNCTIONS,
+    _SPECIAL_CHAR,
+    _NUM,
+};
+
 bool oled_task_kb(void) {
     if (!oled_task_user()) {
         return false;
     }
-    oled_write_P(PSTR("Avalanche\nVersion 4"), false);
+    oled_write_P(PSTR("Daniels tastatur\n\n"), false);
+    oled_write_P(PSTR("Ord per minutt: "), false);
+
+    oled_write(get_u8_str(get_current_wpm(), ' '), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("\n\nLag: Vanlig"), false);
+            break;
+        case _FUNCTIONS:
+            oled_write_P(PSTR("\n\nLag: Piler osv"), false);
+            break;
+        case _SPECIAL_CHAR:
+            oled_write_P(PSTR("\n\nLag: Spesiategn"), false);
+            break;
+        case _NUM:
+            oled_write_P(PSTR("\n\nLag: Tall"), false);
+            break;
+        default:
+            oled_write_P(PSTR("\n\nLag: Ukjent"), false);
+            break;
+    }
+
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.caps_lock ? PSTR("\n\nCAPS LOCK") : PSTR("\n\nikke caps"), false);
+
     return true;
 }
 #endif
